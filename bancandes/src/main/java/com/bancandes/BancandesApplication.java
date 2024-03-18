688114494bc790d16ca4ea7c63aad32457de3ec4
Dav1d10 <d.carol@uniandes.edu.co>
2024-03-18 12:45:45 -0500

    cambios

---
+++ bancandes/src/main/java/com/bancandes/BancandesApplication.java
@@ -0,0 +1,13 @@
+package com.bancandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancandesApplication 
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(BancandesApplication.class, args);
+    }
+    
+}
---
+++ bancandes/src/test/java/com/bancandes/BancandesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bancandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BancandesApplicationTests 
+{
+   
+    @Test
+    void contextLoads() {
+
+    }
+    
+}
