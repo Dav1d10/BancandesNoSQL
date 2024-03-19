1f9703b9c28b4926a57e4c646e56e8c7ee058315
alejandroabrilm <d.abrilm@uniandes.edu.co>
2024-03-18 21:35:42 -0500

    cambios

---
+++ bancandes/src/main/java/com/bancandes/controllers/CajeroController.java
@@ -0,0 +1,40 @@
+package com.bancandes.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bancandes.entities.CajeroEntity;
+import com.bancandes.repository.CajeroRepository;
+
+
+@Controller
+public class CajeroController {
+    
+
+    @Autowired
+    private CajeroRepository cajeroRepository;
+
+
+    
+    @GetMapping("/cajeros/new")
+    public String cajeroForm(Model model) {
+        model.addAttribute("cajero", new CajeroEntity(null, null, null, null, null, null, null, null, null, null));
+        return "cajero";
+    }
+    
+
+    @PostMapping("/cajeros/new/save")
+    public String clienteGuardar(@ModelAttribute CajeroEntity cajero) {
+        cajeroRepository.insertarCajero(cajero.getNumDocumento(), cajero.getTelefono(), cajero.getNombre(), 
+        cajero.getNacionalidad(), cajero.getDireccionFisica(), cajero.getDireccionElectronica(), cajero.getDepartamento(), cajero.getCodigoPostal(),
+        cajero.getCiudad(), cajero.getTipoDocumento());
+        
+        return "redirect:/cajeros";
+    }
+    
+}
+
--- bancandes/src/main/java/com/bancandes/repository/GerenteOficinaRepository.java
+++ bancandes/src/main/java/com/bancandes/repository/GerenteOficinaRepository.java
@@ -14,7 +14,7 @@ public interface GerenteOficinaRepository extends JpaRepository<GerenteOficina,I
     @Modifying
     @Transactional
     @Query(value = "INSERT INTO gerenteOficina (numDocumento ,telefono ,nombre ,nacionalidad, direccionFisica ,direccionElectronica ,departamento ,codigoPostal, ciudad, tipoDocumento)")
-    void insertarGerenteGeneral(@Param("numDocumento") Integer numDocumento, @Param("telefono") String telefono, @Param("nombre")String nombre,
+    void insertarGerenteOficina(@Param("numDocumento") Integer numDocumento, @Param("telefono") String telefono, @Param("nombre")String nombre,
     @Param("nacionalidad")String nacionalidad, @Param("direccionFisica") String direccionFisica, @Param("direccionElectronica")String direccionElectronica,
     @Param("departamento")String departamento, @Param("codigoPostal") String codigoPostal,  @Param("ciudad")String ciudad, @Param("tipoDocumento")TipoDocumento tipoDocumento);
 }
