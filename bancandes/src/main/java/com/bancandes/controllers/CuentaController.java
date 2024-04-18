327736e2c37622872b407997bcea7b0c5e57f066
Dav1d10 <d.carol@uniandes.edu.co>
2024-04-18 17:45:29 -0500

    cambiops

--- bancandes/src/main/java/com/bancandes/controllers/CuentaController.java
+++ bancandes/src/main/java/com/bancandes/controllers/CuentaController.java
@@ -5,28 +5,64 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.bancandes.entities.CuentaEntity;
 import com.bancandes.repository.CuentaRepository;
 
-@RestController
+@Controller
 public class CuentaController {
     
     @Autowired
     private CuentaRepository cuentaRepository;
 
+
+    @GetMapping("/cuentas")
+    public String cuentas(Model model) {
+        model.addAttribute("cuentas", cuentaRepository.darCuentas());
+        return "cuentas";
+    }
+
     @GetMapping("/cuentas/new")
     public String cuentaForm(Model model) {
         model.addAttribute("cuenta", new CuentaEntity());
-        return model.toString();
+        return "cuentaNuevo";
     }
 
     @PostMapping("/cuentas/new/save")
-    public String PuntoAtencionGuardar(@ModelAttribute CuentaEntity cuenta) {
-        cuentaRepository.insertarCuenta(cuenta.getSaldo(), cuenta.getFecha_ultima_transaccion(), cuenta.getFecha_creacion(), cuenta.getTipo_cuenta(), cuenta.getEstado_cuenta());
-        
+    public String cuentaGuardar(@ModelAttribute CuentaEntity cuenta) {
+        cuentaRepository.insertarCuenta(cuenta.getSaldo(), 
+        cuenta.getFecha_ultima_transaccion(), 
+        cuenta.getFecha_creacion(), 
+        cuenta.getTipo_cuenta(), 
+        cuenta.getEstado_cuenta());
+        return "redirect:/cuentas";
+    }
+
+    @GetMapping("/cuentas/{numero_cuenta}/edit")
+    public String cuentaEditarForm(@PathVariable("numero_cuenta") int id, Model model) {
+        CuentaEntity cuenta = cuentaRepository.darCuenta(id);
+        if (cuenta != null) {
+            model.addAttribute("cuenta", cuenta);
+            return "cuentaEditar";
+        } else {
+            return "redirect:/cuentas";
+        }
+    }
+
+    @PostMapping("/cuentas/{numero_cuenta}/edit/save")
+    public String cuentaEditarGuardar(@PathVariable("numero_cuenta") int id, @ModelAttribute CuentaEntity cuenta) {
+        cuentaRepository.actualizarCuenta(cuenta.getSaldo(), 
+        cuenta.getFecha_ultima_transaccion(), 
+        cuenta.getFecha_creacion(), 
+        cuenta.getTipo_cuenta(), 
+        cuenta.getEstado_cuenta());
+        return "redirect:/cuentas";
+    }
+
+    @GetMapping("/cuentas/{numero_cuenta}/delete")
+    public String cuentaEliminar(@PathVariable("numero_cuenta") int id) {
+        cuentaRepository.eliminarCuenta(id);
         return "redirect:/cuentas";
     }
     
--- bancandes/src/main/java/com/bancandes/repository/CuentaRepository.java
+++ bancandes/src/main/java/com/bancandes/repository/CuentaRepository.java
@@ -1,7 +1,8 @@
 package com.bancandes.repository;
 
+import java.sql.Date;
 import java.util.Collection;
-import java.util.Date;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.bancandes.entities.ClienteEntity;
+
 import com.bancandes.entities.CuentaEntity;
 
 
@@ -19,7 +20,7 @@ public interface CuentaRepository extends JpaRepository<CuentaEntity, Integer> {
     Collection<CuentaEntity> darCuentas();
 
     @Query(value = "SELECT * FROM cuenta WHERE numero_cuenta=:numero_cuenta", nativeQuery = true)
-    ClienteEntity darCuenta(@Param("numero_cuenta") int numero_cuenta);
+    CuentaEntity darCuenta(@Param("numero_cuenta") int numero_cuenta);
 
     @Modifying
     @Transactional
@@ -30,4 +31,9 @@ public interface CuentaRepository extends JpaRepository<CuentaEntity, Integer> {
     @Transactional
     @Query(value = "UPDATE cuentas SET saldo=:saldo, fecha_ultima_transaccion=:fecha_ultima_transaccion, fecha_creacion=:fecha_creacion, tipo_cuenta=:tipo_cuenta, estado_cuenta=:estado_cuenta WHERE numero_cuenta=:numero_cuenta", nativeQuery = true)
     void actualizarCuenta(@Param("saldo") Integer saldo, @Param("fecha_ultima_transaccion") Date fecha_ultima_transaccion, @Param("fecha_creacion") Date fecha_creacion, @Param("tipo_cuenta") CuentaEntity.TipoCuenta tipo_cuenta, @Param("estado_cuenta") CuentaEntity.EstadoCuenta estado_cuenta);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM CUENTA WHERE numero_cuenta = :numero_cuenta", nativeQuery = true)
+    void eliminarCuenta(@Param("numero_cuenta") int numero_cuenta);
 }
