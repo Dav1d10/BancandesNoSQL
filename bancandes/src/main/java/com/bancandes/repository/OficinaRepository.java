12600c33575cc3197c5236912b21a1540c04d559
glmartinez-prog <gl.martinez@uniandes.edu.co>
2024-04-24 19:54:53 -0500

    Actualizacion oficina

--- bancandes/src/main/java/com/bancandes/repository/OficinaRepository.java
+++ bancandes/src/main/java/com/bancandes/repository/OficinaRepository.java
@@ -1,5 +1,7 @@
 package com.bancandes.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,10 +11,26 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bancandes.entities.OficinaEntity;
 
 public interface OficinaRepository extends JpaRepository<OficinaEntity, Integer> {
+
+    @Query(value = "SELECT * FROM OFICINA", nativeQuery = true)
+    Collection<OficinaEntity> darOficinas();
+
+    @Query(value = "SELECT * FROM OFICINA WHERE id_oficina= :id_oficina", nativeQuery = true)
+    OficinaEntity darOficina(@Param("id_oficina") int id_oficina);
     
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO oficinas (id, nombre, direccion, puntos_atencion) VALUES(bancandes_sequence.nextval, :nombre, :direccion, :puntos_atencion)", nativeQuery = true)
+    @Query(value = "INSERT INTO OFICINA (id, nombre, direccion, puntos_atencion) VALUES(bancandes_sequence.nextval, :nombre, :direccion, :puntos_atencion)", nativeQuery = true)
     void insertarOficina(@Param("nombre") String nombre, @Param("direccion") String direccion, @Param("puntos_atencion") Integer puntos_atencion);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE OFICINA SET nombre=:nombre, direccion=:direccion, puntos_atencion=:puntos_atencion WHERE id_oficina=:id_oficina", nativeQuery =true)
+    void actualizarOficina(@Param("nombre") String nombre, @Param("direccion") String direccion, @Param("puntos_atencion") Integer puntos_atencion);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM OFICINA WHERE id_oficina = :id_oficina", nativeQuery = true)
+    void eliminarOficina(@Param("id_oficina") int id_oficina);
 
 }
