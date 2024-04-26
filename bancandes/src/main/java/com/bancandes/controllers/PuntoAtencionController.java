4aa4cede43d1f955e7ffafe0a147bc0771bc9825
David Caro <d.carol@uniandes.edu.co>
2024-04-25 19:01:29 -0500

    cambios

--- bancandes/src/main/java/com/bancandes/controllers/PuntoAtencionController.java
+++ bancandes/src/main/java/com/bancandes/controllers/PuntoAtencionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import com.bancandes.entities.PuntoAtencionEntity;
 import com.bancandes.repository.PuntoAtencionRepository;
@@ -18,57 +18,46 @@ public class PuntoAtencionController {
     @Autowired
     private PuntoAtencionRepository puntoAtencionRepository;
 
-    @GetMapping("/puntosAtencion")
+    @GetMapping("/puntosatencion")
     public String puntosAtencion(Model model) {
-        model.addAttribute("puntosAtencion", puntoAtencionRepository.darPuntosAtencion());
-        return "puntosAtencion";
+        model.addAttribute("puntosatencion", puntoAtencionRepository.darPuntosAtencion());
+        return "puntosatencion";
     }
 
-    @GetMapping("/puntosAtencion/new")
+    @GetMapping("/puntosatencion/new")
     public String puntoAtencionForm(Model model) {
-        model.addAttribute("puntoAtencion", new PuntoAtencionEntity());
-        return "puntoAtencionForm";
+        model.addAttribute("puntoatencion", new PuntoAtencionEntity());
+        return "puntoAtencionNuevo";
     }
 
-    @PostMapping("/puntosAtencion/new/save")
-    public String guardarPuntoAtencion(@ModelAttribute PuntoAtencionEntity puntoAtencion, RedirectAttributes redirectAttributes) {
-        try {
-            puntoAtencionRepository.insertarPuntoAtencion(
-                                                         puntoAtencion.getLocalizacion(),
-                                                         puntoAtencion.getTipo_punto_atencion());
-            redirectAttributes.addFlashAttribute("successMessage", "Punto de atención creado exitosamente.");
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Error al crear el punto de atención.");
-        }
-        return "redirect:/puntosAtencion";
+    @PostMapping("/puntosatencion/new/save")
+    public String guardarPuntoAtencion(@ModelAttribute PuntoAtencionEntity puntoAtencion) {
+        puntoAtencionRepository.insertarPuntoAtencion(puntoAtencion.getLocalizacion(),
+        puntoAtencion.getTipo_punto_atencion().name());
+        return "redirect:/puntosatencion";
     }
 
-    @GetMapping("/puntosAtencion/{id_punto_atencion}/edit")
+    @GetMapping("/puntosatencion/{id_punto_atencion}/edit")
     public String editarPuntoAtencion(@PathVariable("id_punto_atencion") int id, Model model) {
         PuntoAtencionEntity puntoAtencion = puntoAtencionRepository.darPuntoAtencion(id);
         if (puntoAtencion != null) {
-            model.addAttribute("puntoAtencion", puntoAtencion);
-            return "editarPuntoAtencion";
+            model.addAttribute("puntoatencion", puntoAtencion);
+            return "puntoAtencionEditar";
         } else {
-            return "redirect:/puntosAtencion";
+            return "redirect:/puntosatencion";
         }
     }
 
-    @PostMapping("/puntosAtencion/{id_punto_atencion}/edit/save")
+    @PostMapping("/puntosatencion/{id_punto_atencion}/edit/save")
     public String editarGuardarPuntoAtencion(@PathVariable("id_punto_atencion") int id, @ModelAttribute PuntoAtencionEntity puntoAtencion) {
-        puntoAtencionRepository.actualizarPuntoAtencion(puntoAtencion.getLocalizacion(),
-                                                        puntoAtencion.getTipo_punto_atencion());
-        return "redirect:/puntosAtencion";
+        puntoAtencionRepository.actualizarPuntoAtencion(id, puntoAtencion.getLocalizacion(),
+                                                        puntoAtencion.getTipo_punto_atencion().name());
+        return "redirect:/puntosatencion";
     }
 
-    @GetMapping("/puntosAtencion/{id_punto_atencion}/delete")
-    public String eliminarPuntoAtencion(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
-        try {
-            puntoAtencionRepository.eliminarPuntoAtencion(id);
-            redirectAttributes.addFlashAttribute("successMessage", "Punto de atención eliminado exitosamente.");
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("errorMessage", "No se puede eliminar el punto de atención, ya que tiene operaciones asociadas.");
-        }
-        return "redirect:/puntosAtencion";
+    @GetMapping("/puntosatencion/{id_punto_atencion}/delete")
+    public String eliminarPuntoAtencion(@PathVariable("id_punto_atencion") int id) {
+        puntoAtencionRepository.eliminarPuntoAtencion(id);
+        return "redirect:/puntosatencion";
     }
 }
