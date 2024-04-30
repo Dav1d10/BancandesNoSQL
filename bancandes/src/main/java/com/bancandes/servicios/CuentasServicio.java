6de75951a94d625ed1d02162859d92b9befb4772
Dav1d10 <d.carol@uniandes.edu.co>
2024-04-29 23:14:05 -0500

    cambios

--- bancandes/src/main/java/com/bancandes/servicios/CuentasServicio.java
+++ bancandes/src/main/java/com/bancandes/servicios/CuentasServicio.java
@@ -1,27 +1,43 @@
 package com.bancandes.servicios;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bancandes.entities.CuentaEntity;
 import com.bancandes.repository.CuentaRepository;
+import com.bancandes.repository.OperacionBancariaRepository;
 
 @Service
 public class CuentasServicio {
 
     private CuentaRepository cuentaRepository;
 
+    private OperacionBancariaRepository operacionBancariaRepository;
 
-    public CuentasServicio(CuentaRepository cuentaRepository) {
+     
+    
+    public CuentasServicio(CuentaRepository cuentaRepository, OperacionBancariaRepository operacionBancariaRepository) {
         this.cuentaRepository = cuentaRepository;
+        this.operacionBancariaRepository = operacionBancariaRepository;
     }
+
+
 
     @Transactional(isolation = Isolation.SERIALIZABLE, rollbackFor = Exception.class)
     public void consignacionCuentaSerializable(int numero_cuenta, int cantidad_consignacion) {
         try {
+            LocalDate fechaActual = LocalDate.now();
+            LocalTime horaActual = LocalTime.now();
+            Date fechaSql = Date.valueOf(fechaActual);
             CuentaEntity cuentaActual = cuentaRepository.darCuenta(numero_cuenta);
             cuentaRepository.actualizarCuenta(numero_cuenta, cuentaActual.getSaldo() + cantidad_consignacion, cuentaActual.getFecha_ultima_transaccion(), cuentaActual.getFecha_creacion(), cuentaActual.getTipo_cuenta().name(), cuentaActual.getEstado_cuenta().name());
+            operacionBancariaRepository.insertarOperacionBancaria(cantidad_consignacion, horaActual.format(DateTimeFormatter.ofPattern("HH:mm:ss")), fechaSql, "CUENTA", "CONSIGNACION");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
